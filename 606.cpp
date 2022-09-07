ba3547c53a3f64022b88af4432fa1b44224d8796
MicheleFiladelfia <michele270904@gmail.com>
2022-09-07 13:19:31 +0200

    feat: 606

---
+++ 606.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(root == nullptr)
+            return "";
+        
+        string ans = to_string(root -> val);
+        
+        if(root -> left){
+            ans += "(" + tree2str(root -> left) + ")";
+        }
+        
+        if(root -> right){
+            if(!root -> left){
+                ans += "()";
+            }
+            
+            ans += "(" + tree2str(root -> right) + ")";
+        }
+        
+        return ans;
+    }
+};
