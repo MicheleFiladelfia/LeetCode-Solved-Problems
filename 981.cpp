02c6e855da3aaf7b6ccbcc53833c507c3ff55f37
MicheleFiladelfia <michele270904@gmail.com>
2022-10-06 17:35:13 +0200

    feat: 981

---
+++ 981.cpp
@@ -0,0 +1,24 @@
+class TimeMap {
+public:
+   unordered_map<string, map<int, string, greater<int>>> m;
+
+   TimeMap() {}
+
+   void set(string key, string value, int timestamp) {
+      m[key][timestamp] = value;
+   }
+
+   string get(string key, int timestamp) {
+      auto i = m.find(key);
+      if (i == m.end()) return "";
+      auto j = i->second.lower_bound(timestamp);
+      return j != i->second.end() ? j->second : "";
+   }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
